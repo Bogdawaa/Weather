06acec5764099ce874a594cb9b9530685fb9a92f
Bogdawaa <bogdan6246@yandex.ru>
2025-06-02 16:03:30 +0300

    feat: задана rootView

--- Weather/SceneDelegate.swift
+++ Weather/SceneDelegate.swift
@@ -15,7 +15,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = MainViewController()
+        
+        var presenter: MainPresenter = MainPresenterImpl()
+        let vc = MainViewController(presenter: presenter)
+        presenter.view = vc
+        
+        window?.rootViewController = vc
         window?.makeKeyAndVisible()
     }
 }
