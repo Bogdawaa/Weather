f32ce811f4bab0c9ba512141230c799e1667a12e
Bogdawaa <bogdan6246@yandex.ru>
2025-06-03 13:46:15 +0300

    feat: добавлена функция фильтрации элементов типа HourForecast

--- Weather/Helpers/Array+Extensions.swift
+++ Weather/Helpers/Array+Extensions.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+extension Array where Element == HourForecast {
+    
+    func filteredForCurrentAndNextDay() -> [HourForecast] {
+        let calendar = Calendar.current
+        let now = Date()
+        
+        let currentHour = calendar.dateComponents([.year, .month, .day, .hour], from: now)
+        guard let currentHourStart = calendar.date(from: currentHour) else {
+            return []
+        }
+        
+        guard let nextDayStart = calendar.date(byAdding: .day, value: 1, to: now) else {
+            return []
+        }
+        let nextDayComponents = calendar.dateComponents([.year, .month, .day], from: nextDayStart)
+        guard let nextDayMidnight = calendar.date(from: nextDayComponents) else {
+            return []
+        }
+        
+        guard let nextDayEnd = calendar.date(byAdding: .day, value: 1, to: nextDayMidnight) else {
+            return []
+        }
+        
+        return self.filter { hour in
+            let hourDate = Date(timeIntervalSince1970: TimeInterval(hour.timeEpoch))
+            return hourDate >= currentHourStart && hourDate < nextDayEnd
+        }
+    }
+}
